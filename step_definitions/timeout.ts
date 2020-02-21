bffcb68a66c532167b548315331724eef1e5d99f
KennethMercado <Kenita-PC+KennethMercado@users.noreply.github.com>
2020-02-21 23:28:29 +0000

    Updated Timeout

--- step_definitions/timeout.ts
+++ step_definitions/timeout.ts
@@ -1,4 +1,5 @@
 //Reference: https://github.com/cucumber/cucumber-js/blob/master/docs/support_files/timeouts.md
+//Set Global Timeouts
 var {setDefaultTimeout} = require('cucumber');
 
 setDefaultTimeout(60 * 1000);
--- timeout.ts
+++
@@ -1,4 +0,0 @@
-//Reference: https://github.com/cucumber/cucumber-js/blob/master/docs/support_files/timeouts.md
-var {setDefaultTimeout} = require('cucumber');
-
-setDefaultTimeout(60 * 1000);
